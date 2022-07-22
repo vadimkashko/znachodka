2be7913b357300ad69a15c1ce83811cee6375ec7
Vadzim Kashko <vadim.kashko@gmail.com>
2022-07-23 00:51:11 +0300

    Templates added, views and urls modified

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,20 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.template import loader
+
+from catalog.models import Item
 
 
 def index(request):
-    return HttpResponse('<h1>Home</h1>')
+    items_list = Item.objects.all()
+
+    template = loader.get_template('catalog/index.html')
+    context = {
+        'items_list': items_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def item(request, item_id):
+    item = Item.objects.get(id=item_id)
+    resp = f"<h1>Item:</h1><ul>{item.__dict__}</ul>"
+    return HttpResponse(resp)
